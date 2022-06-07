69ffb574dec71618074af1f4e1215c40f7b90348
hatem-salem <58885673+hatem-salem@users.noreply.github.com>
2022-06-07 13:00:06 +0200

    first commit

---
+++ 4-names.js
@@ -0,0 +1,12 @@
+//local
+const secret = "secret";
+//global
+const john = "John";
+const jane = "Jane";
+const mary = "Mary";
+
+// console.log(module);
+// console.log(process)
+// console.log(require);
+
+module.exports = { john, jane, mary };
